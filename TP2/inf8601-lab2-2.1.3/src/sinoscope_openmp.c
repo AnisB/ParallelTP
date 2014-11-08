967590f515406b5af5ea2061a987c90721b36265
Matthieu Courbariaux <macouaj@l4712-13.info.polymtl.ca>
2014-11-08 17:40:02 -0500

    openmp sinoscope with noise

--- TP2/inf8601-lab2-2.1.3/src/sinoscope_openmp.c
+++ TP2/inf8601-lab2-2.1.3/src/sinoscope_openmp.c
@@ -16,38 +16,36 @@
 int sinoscope_image_openmp(sinoscope_t *ptr)
 {
     if (ptr == NULL)
-        return -1;
-
-    sinoscope_t b = *ptr;
-	int x, y;
-	#pragma omp parallel private(x,y)
-	for(x = 1; x < b.width-1; x++)
+        return -1;	
+	
+	#pragma omp parallel 
 	{
-	    int index, taylor;
+		sinoscope_t b = *ptr;
 		struct rgb c;
+		int x, y, index, taylor;
 		float val, px, py;
-		for(y = 1; y < b.height-1; y++)
+		
+		#pragma omp for
+		for(x=1;x < b.width-1;x++)
 		{
-            px = b.dx * y - 2 * M_PI;
-            py = b.dy * x - 2 * M_PI;
-            val = 0.0f;
-            for (taylor = 1; taylor <= b.taylor; taylor += 2) 
-            {
-                val += sin(px * taylor * b.phase1 + b.time) / taylor + cos(py * taylor * b.phase0) / taylor;
-            }
-            val = (atan(1.0 * val) - atan(-1.0 * val)) / (M_PI);
-            val = (val + 1) * 100;
-            value_color(&c, val, b.interval, b.interval_inv);
-            index = (y * 3) + (x * 3) * b.width;
-            #pragma omp critical
-            {
-                b.buf[index + 0] = c.r;
-		        b.buf[index + 1] = c.g;
-		        b.buf[index + 2] = c.b;
-            }
+			for(y=1;y < b.height-1;y++)
+			{
+				px = b.dx * y - 2 * M_PI;
+				py = b.dy * x - 2 * M_PI;
+				val = 0.0f;
 
-        }
-	}
-        
+				for (taylor = 1; taylor <= b.taylor; taylor += 2) {
+				    val += sin(px * taylor * b.phase1 + b.time) / taylor + cos(py * taylor * b.phase0) / taylor;
+				}
+				val = (atan(1.0 * val) - atan(-1.0 * val)) / (M_PI);
+				val = (val + 1) * 100;
+				value_color(&c, val, b.interval, b.interval_inv);
+				index = (y * 3) + (x * 3) * b.width;
+				b.buf[index + 0] = c.r;
+				b.buf[index + 1] = c.g;
+				b.buf[index + 2] = c.b;
+			}
+		}
+    }
     return 0;
 }

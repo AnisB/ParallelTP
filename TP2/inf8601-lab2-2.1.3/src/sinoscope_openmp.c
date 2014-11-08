53bf27cec8f4c361331f2ce9448a129a36b87ebf
Matthieu Courbariaux <macouaj@l4712-13.info.polymtl.ca>
2014-11-08 17:49:51 -0500

    correction openmp

--- TP2/inf8601-lab2-2.1.3/src/sinoscope_openmp.c
+++ TP2/inf8601-lab2-2.1.3/src/sinoscope_openmp.c
@@ -33,10 +33,11 @@ int sinoscope_image_openmp(sinoscope_t *ptr)
 				px = b.dx * y - 2 * M_PI;
 				py = b.dy * x - 2 * M_PI;
 				val = 0.0f;
-
+		
 				for (taylor = 1; taylor <= b.taylor; taylor += 2) {
-				    val += sin(px * taylor * b.phase1 + b.time) / taylor + cos(py * taylor * b.phase0) / taylor;
+					val += sin(px * taylor * b.phase1 + b.time) / taylor + cos(py * taylor * b.phase0) / taylor;
 				}
+		
 				val = (atan(1.0 * val) - atan(-1.0 * val)) / (M_PI);
 				val = (val + 1) * 100;
 				value_color(&c, val, b.interval, b.interval_inv);
@@ -44,6 +45,7 @@ int sinoscope_image_openmp(sinoscope_t *ptr)
 				b.buf[index + 0] = c.r;
 				b.buf[index + 1] = c.g;
 				b.buf[index + 2] = c.b;
+
 			}
 		}
     }
